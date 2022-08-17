49199813c4b01b64f5e7ecf1a76bc81db5555d5f
yuyue-creater <michaelyuyue2012@gmail.com>
2022-08-17 16:11:48 -0700

    adapter and some age calculations included

--- src/components/controls/DatePicker.js
+++ src/components/controls/DatePicker.js
@@ -17,7 +17,7 @@ export default function DatePicker(props) {
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <KeyboardDatePicker disableToolbar variant="inline" inputVariant="outlined"
                 label={label}
-                format="MMM/dd/yyyy"
+                format="yyyy/MMM/dd"
                 name={name}
                 value={value}
                 onChange={date =>onChange(convertToDefEventPara(name,date))}
